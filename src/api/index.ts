08c5bb74c1e52acf8c08ea376eb570b34193c175
LehaBiceps <alexey005v@gmail.com>
2025-10-31 09:10:06 +0700

    1.2

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,12 @@ type RESPONSE_DATA = {
 };
 
 const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
-  const path = `http://localhost:3001/api/${endpoint}`;
+  const path = 
+    process.env.NODE_ENV === 'development'
+    ? `http://localhost:3001/api/${endpoint}`
+    : 'https://raw.githubusercontent.com/LehaBiceps/DockerPract/refs/heads/docker-7/src/server/db/data.json'
+  ;
+
   const response = await fetch(path);
   return await response.json();
 };
